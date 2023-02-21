c7652306ce0c54a8533f789a9c768ff05060df70
SaiAshish <saiashish3760@gmail.com>
2023-02-22 03:19:10 +0530

    Add files via upload

---
+++ nexus-artifactory/src/main/java/com/sai/nexusartifactory/NexusArtifactoryApplication.java
@@ -0,0 +1,12 @@
+package com.sai.nexusartifactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class NexusArtifactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusArtifactoryApplication.class, args);
+	}
+
+}
